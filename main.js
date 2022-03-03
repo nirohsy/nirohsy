111bd82f55eb93f11832fd2f043765b5a3dca3e5
shorin <11shorin@gmail.com>
2022-03-04 00:00:26 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,67 @@
+
+$(window).on('load',function(){
+    $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#loading_box").delay(1200).fadeOut('slow');//ローディング画像を1.2秒（1200ms）待機してからフェードアウト
+  });
+
+  
+
 $('.menu-btn').on('click',function(){
     $('.menu-btn').toggleClass('active');
     $('.nav').fadeToggle();
     $('.page-main').fadeToggle();
     $('html,body').animate({scrollTop:0}, 'fast');
+    $('.page-nav figure').css('opacity' , '50%');
 });
 $('.lo01').hover(
     function(){
-    $('.pho01 , .pho01-2').css('opacity' , '100%')
+    $('.pho01 , .pho01-2').css('opacity' , '100%');
     },
     function(){
-    $('.pho01 , .pho01-2').css('opacity' , '50%')
+    $('.pho01 , .pho01-2').css('opacity' , '50%');
 });
 $('.lo02').hover(
     function(){
-    $('.pho02 , .pho02-2').css('opacity' , '100%')
+    $('.pho02 , .pho02-2').css('opacity' , '100%');
     },
     function(){
-    $('.pho02 , .pho02-2').css('opacity' , '50%')
+    $('.pho02 , .pho02-2').css('opacity' , '50%');
 });
 $('.lo03').hover(
     function(){
-    $('.pho03').css('opacity' , '100%')
+    $('.pho03').css('opacity' , '100%');
     },
     function(){
-    $('.pho03').css('opacity' , '50%')
+    $('.pho03').css('opacity' , '50%');
 });
 $('.lo04').hover(
     function(){
-    $('.pho04').css('opacity' , '100%')
+    $('.pho04').css('opacity' , '100%');
     },
     function(){
-    $('.pho04').css('opacity' , '50%')
+    $('.pho04').css('opacity' , '50%');
 });
 $('.lo05 , lo05-2').hover(
     function(){
-    $('.pho05').css('opacity' , '100%')
+    $('.pho05').css('opacity' , '100%');
     },
     function(){
-    $('.pho05').css('opacity' , '50%')
+    $('.pho05').css('opacity' , '50%');
+});
+
+
+$('.lo01').click(function() {
+    $('.pho01 , .pho01-2').css('opacity' , '100%');
+});
+$('.lo02').click(function() {
+    $('.pho02 , .pho02-2').css('opacity' , '100%');
+});
+$('.lo03').click(function() {
+    $('.pho03').css('opacity' , '100%');
+});
+$('.lo04').click(function() {
+    $('.pho04').css('opacity' , '100%');
+});
+$('.lo05 , lo05-2').click(function() {
+    $('.pho05').css('opacity' , '100%');
 });
